1da93e6ba09a48f1946e6898f5876efe0b02274b
FreyaJiang0104 <144355063+FreyaJiang0104@users.noreply.github.com>
2024-11-10 22:49:29 -0500

    Update deploy-elt.py

--- prefect/flows/deploy-elt.py
+++ prefect/flows/deploy-elt.py
@@ -2,13 +2,13 @@ from prefect import flow
 
 if __name__ == "__main__":
     flow.from_source(
-        source="https://github.com/SnehaEkka/BA882-Netflix-Analytics-Pipeline.git",
-        entrypoint="flows/elt.py:elt_flow",
+        source="https://github.com/SnehaEkka/BA882-Team05-Project.git",
+        entrypoint="/home/sekka/BA882-Team05-Project/flows/elt.py:elt_flow",
     ).deploy(
         name="ba882-project-elt",
         work_pool_name="ba882-05-pool",
         job_variables={"pip_packages": ["pandas", "requests"]},
-        cron="35 22 * * *",  # cron for daily at 5 AM
+        cron="0 5 * * *",  # cron for daily at 5 AM
         tags=["prod"],
         description="Pipeline to populate Netflix incoming data daily",
         version="1.0.0",
